d1b68d5285f0c0a5d37fe5a2043e209c6ced3cc6
JYH <salvayh@thingspire.com>
2023-08-02 15:59:40 +0900

    webpack.config에 대한 설명/주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require("path");
-
+//const React = require();
 //var webpack = require("webpack");
 //var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "development", //production
+  //devtool : 'eval', //hidden-source-map
   //plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+  resolve: {
+    extensions: ["js", "jsx"],
+  },
   entry: {
+    //입력
     core: "./core/index.js",
-    prj1: "./prj1/src/index.js",
-    prj2: "./prj2/src/index.js",
+    prj1: "./prj1/src/index.js", //확장자 안써도 됨. 위에 resolve에 넣으면 알아서 찾아줌.
+    prj2: "./prj2/src/index.js", //만약 prj2에 prj1이 있으면 prj1은 안써도됨.
   },
   output: {
+    //출력
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"), // 현재폴더 기준이기 때문에 각각 프로젝트에 webpack.config.js 넣어도 될듯함.
   },
   module: {
     rules: [
@@ -23,7 +29,8 @@ module.exports = {
         use: {
           loader: "babel-loader", // babel-loader 추가
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ["@babel/preset-env", "@babel/preset-react"], //프리셋은 플러그인의 모음이기 때문에 각 프리셋에 옵션도 줄수 있음. (ex. last 2 chrome)
+            //plugins : [기본적으로 프리셋만 하고 오류 발생하면 플러그인도 추가],
           },
         },
       },
